6635a9a45f26db2820ecf666debdc06683c5dada
ahmad amira <ahmad_amira@outlook.com>
2023-05-15 22:06:30 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState({
+    title: '',
+    description: '',
+    completed: false
+  });
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/todos');
+      setTodos(response.data);
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+    }
+  };
+
+  const createTodo = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`http://localhost:3000/todos/${Date.now()}`, newTodo);
+      setNewTodo({
+        title: '',
+        description: '',
+        completed: false
+      });
+      fetchTodos();
+    } catch (error) {
+      console.error('Error creating todo:', error);
+    }
+  };
+
+  const updateTodo = async (id, updatedTodo) => {
+    try {
+      await axios.put(`http://localhost:3000/todos/${id}`, updatedTodo);
+      fetchTodos();
+    } catch (error) {
+      console.error('Error updating todo:', error);
+    }
+  };
+
+  const deleteTodo = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3000/todos/${id}`);
+      fetchTodos();
+    } catch (error) {
+      console.error('Error deleting todo:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <form onSubmit={createTodo}>
+        <input
+          type="text"
+          placeholder="Title"
+          value={newTodo.title}
+          onChange={(e) => setNewTodo({ ...newTodo, title: e.target.value })}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Description"
+          value={newTodo.description}
+          onChange={(e) => setNewTodo({ ...newTodo, description: e.target.value })}
+          required
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="text"
+              value={todo.title}
+              onChange={(e) => updateTodo(todo.id, { ...todo, title: e.target.value })}
+            />
+            <input
+              type="text"
+              value={todo.description}
+              onChange={(e) => updateTodo(todo.id, { ...todo, description: e.target.value })}
+            />
+            <label>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={(e) => updateTodo(todo.id, { ...todo, completed: e.target.checked })}
+              />
+              Completed
+            </label>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
